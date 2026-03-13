9e1c3a9e1e88373b3e76e8642be8e1a4e4f9450f
tionofficialwork <nechaev.of.work@gmail.com>
2026-03-13 19:04:36 +0300

    feat: add courier map env configuration

--- lib/geocode.ts
+++ lib/geocode.ts
@@ -1,21 +1,35 @@
 export async function geocodeAddress(address: string) {
-  const apiKey = process.env.EXPO_PUBLIC_YANDEX_MAPS_API_KEY
+  try {
+    const apiKey = process.env.EXPO_PUBLIC_YANDEX_MAPS_API_KEY
 
-  const url = `https://geocode-maps.yandex.ru/1.x/?apikey=${apiKey}&format=json&geocode=${encodeURIComponent(
-    address
-  )}`
+    const url = `https://geocode-maps.yandex.ru/1.x/?apikey=${apiKey}&format=json&geocode=${encodeURIComponent(
+      address
+    )}`
 
-  const res = await fetch(url)
+    const res = await fetch(url)
+    const data = await res.json()
 
-  const data = await res.json()
+    const collection =
+      data?.response?.GeoObjectCollection?.featureMember
 
-  const pos =
-    data.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos
+    if (!collection || collection.length === 0) {
+      throw new Error("Address not found")
+    }
 
-  const [lon, lat] = pos.split(" ").map(Number)
+    const pos = collection[0].GeoObject.Point.pos
 
-  return {
-    latitude: lat,
-    longitude: lon
+    const [lon, lat] = pos.split(" ").map(Number)
+
+    return {
+      latitude: lat,
+      longitude: lon
+    }
+  } catch (error) {
+    console.log("Geocode error:", error)
+
+    return {
+      latitude: 55.751244,
+      longitude: 37.618423
+    }
   }
 }
